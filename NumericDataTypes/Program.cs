c4c05cb3d9d5b6ac785ba585e39b5a5b5aeea50a
Fulin <944799613@qq.com>
2022-10-03 14:15:39 -0500

    Add project files.

---
+++ NumericDataTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿//Which scenarios should the decimal data type be used
+//1. Monetary transactions
+//3. Arithmetic that rounds to the decimal place of the most precise number
+//4. Calculating trajectories for smashing two objects in space into each other
+
+decimal a = 9.9m;
+decimal b = 8.8m;
+
+
+void AddFloatingPoints()   
+{
+    Console.WriteLine((double)a + (double)b); 
+}
